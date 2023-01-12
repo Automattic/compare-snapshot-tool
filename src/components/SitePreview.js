c08f24fb8476f4e7475a7ff6bd15b77b549f47d7
dpasque <dan.speckhard.pasque@automattic.com>
2023-01-12 14:51:52 -0600

    Provide reload button for iframes

--- src/components/SitePreview.js
+++ src/components/SitePreview.js
@@ -1,19 +1,29 @@
 import generateMShotsUrl from "../lib/mShots"
+import { useState } from "react";
 
 const Title = (props) => {
     return (
-        <p>
+        <span>
           <strong>PAGE {props.index}: </strong>
           <a href={props.url} target="_blank" rel="noreferrer">{props.url}</a>
-        </p>
+        </span>
     )
 }
 
 const Preview = (props) => {
+	// We need a way to force the iframe to remount in the case where mShots isn't ready and has redirected the iframe.
+	// Easiest way is the key attribute and bumping it with state!
+	// A bit hacky, but probably our best bet given the weird use case.
+	const [iframeKey, setIframeKey] = useState(0);
+	const handleReload = () => setIframeKey(iframeKey + 1);
+
     return (
         <div className="column">
-            <Title {...props}></Title>
-            <iframe title="preview-0" className="preview" src={props.mShotsUrl} width="100%" height="500" />
+			<div className="preview-top-bar">
+				<Title {...props}></Title>
+				<button onClick={handleReload}>Reload</button>
+			</div>
+            <iframe key={iframeKey} title="preview-0" className="preview" src={props.mShotsUrl} width="100%" height="500" />
         </div>
     )
 }
