db69eabfa04edadb12e2fddf0e2c31147dcee58e
karenroldan <mail@karenroldan.net>
2023-01-18 11:01:24 +0100

    Update tool description

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -2,7 +2,7 @@ export default function Instructions() {
     return (
         <div className='instructions'>
             <h2>Upload a CSV to compare</h2>
-            <p>This tool will display a side-by-side snapshot of two URLs</p>
+            <p>This tool can display a side-by-side snapshot or preview of two URLs</p>
         </div>
       )
 }
